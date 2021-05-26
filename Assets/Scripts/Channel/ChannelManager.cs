56443dd4f0bee4e608a0a123307c75edadb5ed75
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-27 05:43:37 +0900

    ・購読解除メソッド定義 ・購読した時、購読を解除した時のObservableを定義

--- Assets/Scripts/Channel/ChannelManager.cs
+++ Assets/Scripts/Channel/ChannelManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using System;
 
 namespace Channel
 {
@@ -15,6 +17,26 @@ namespace Channel
         private Dictionary<string, Channel> ChannelDic = new Dictionary<string, Channel>();
 
         /// <summary>
+        /// 購読時のSubject
+        /// </summary>
+        private Subject<Channel> OnSubscribeSubject = new Subject<Channel>();
+
+        /// <summary>
+        /// 購読した
+        /// </summary>
+        public IObservable<Channel> OnSubscribe { get { return OnSubscribeSubject; } }
+
+        /// <summary>
+        /// 購読解除Subject
+        /// </summary>
+        private Subject<Channel> OnUnsubscribeSubject = new Subject<Channel>();
+
+        /// <summary>
+        /// 購読を解除した
+        /// </summary>
+        public IObservable<Channel> OnUnsubscribe { get { return OnUnsubscribeSubject; } }
+
+        /// <summary>
         /// コンストラクタ
         /// </summary>
         public ChannelManager()
@@ -27,7 +49,21 @@ namespace Channel
         /// <param name="NewChannel">チャンネル</param>
         public void Add(Channel NewChannel)
         {
+            if (Exists(NewChannel.Name)) { return; }
             ChannelDic[NewChannel.Name] = NewChannel;
+            OnSubscribeSubject.OnNext(NewChannel);
+        }
+
+        /// <summary>
+        /// チャンネル削除
+        /// </summary>
+        /// <param name="ChannelName">チャンネル名</param>
+        public void Remove(string ChannelName)
+        {
+            if (!Exists(ChannelName)) { return; }
+            var Ch = ChannelDic[ChannelName];
+            ChannelDic.Remove(ChannelName);
+            OnUnsubscribeSubject.OnNext(Ch);
         }
 
         /// <summary>
