84ebdf1cc4814a629debcf1d867b17298e060302
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-27 06:57:03 +0900

    ロビーメニュークラスとインタフェースを定義

---
+++ Assets/Scripts/UI/Lobby/LobbyMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI.Lobby
+{
+    /// <summary>
+    /// ロビーメニューインタフェース
+    /// </summary>
+    public interface ILobbyMenu
+    {
+    }
+
+    /// <summary>
+    /// ロビーメニュー
+    /// </summary>
+    public class LobbyMenu : MonoBehaviour, ILobbyMenu
+    {
+    }
+}
