bb87e9e0b7e87324b6f3c16460e2e1c3a2925110
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-26 20:14:02 +0900

    ログインボタンをとりあえず作成

---
+++ Assets/Scripts/UI/LogInButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+using System;
+
+namespace UI
+{
+    /// <summary>
+    /// ログインボタンインタフェース
+    /// </summary>
+    public interface ILogInButton
+    {
+        /// <summary>
+        /// 押された
+        /// </summary>
+        IObservable<Unit> OnPress { get; }
+    }
+
+    /// <summary>
+    /// ログインボタン
+    /// </summary>
+    public class LogInButton : MonoBehaviour, ILogInButton
+    {
+        /// <summary>
+        /// ボタン
+        /// ※ネーミングどうにかならない・・・？
+        /// </summary>
+        private Button MyButton = null;
+
+        /// <summary>
+        /// 押された
+        /// </summary>
+        public IObservable<Unit> OnPress { get { return MyButton.OnClickAsObservable(); } }
+
+        void Awake()
+        {
+            MyButton = GetComponent<Button>();
+
+            OnPress
+                .Subscribe((_) => MyButton.interactable = false)
+                .AddTo(gameObject);
+        }
+    }
+}
