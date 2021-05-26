9ddb436bd12b89eda57ec6334700965a9ba5759f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-27 05:33:43 +0900

    チャンネルクラスとその管理クラスをとりあえず定義

---
+++ Assets/Scripts/Channel/Channel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Chat;
+
+namespace Channel
+{
+    /// <summary>
+    /// チャンネルクラス
+    /// </summary>
+    public class Channel
+    {
+        /// <summary>
+        /// チャンネル名
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="Src">Photonが定義したチャンネルクラス</param>
+        public Channel(ChatChannel Src)
+        {
+            Name = Src.Name;
+        }
+    }
+}
---
+++ Assets/Scripts/Channel/ChannelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Channel
+{
+    /// <summary>
+    /// チャンネル管理クラス
+    /// </summary>
+    public class ChannelManager
+    {
+        /// <summary>
+        /// チャンネル名をキーにしたチャンネルのDictionary
+        /// </summary>
+        private Dictionary<string, Channel> ChannelDic = new Dictionary<string, Channel>();
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ChannelManager()
+        {
+        }
+
+        /// <summary>
+        /// チャンネル追加
+        /// </summary>
+        /// <param name="NewChannel">チャンネル</param>
+        public void Add(Channel NewChannel)
+        {
+            ChannelDic[NewChannel.Name] = NewChannel;
+        }
+    }
+}
