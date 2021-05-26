99d30d2be408c643363ebcd24e80cee82d7991a6
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-26 18:16:01 +0900

    PhotonChatと接続するクラスをとりあえず定義

---
+++ Assets/Scripts/Network/ChatConnection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Chat;
+using UniRx;
+using UniRx.Operators;
+
+namespace Network
+{
+    /// <summary>
+    /// PhotonChatとの接続
+    /// </summary>
+    public class ChatConnection
+    {
+        /// <summary>
+        /// 接続
+        /// </summary>
+        public void Connect()
+        {
+        }
+
+        /// <summary>
+        /// 切断
+        /// </summary>
+        public void Disconnect()
+        {
+        }
+
+        #region Singleton
+        public static ChatConnection Intsance { get { return _Instance; } }
+        private static ChatConnection _Instance = new ChatConnection();
+        private ChatConnection() { }
+        #endregion
+    }
+}
