d8891fb26156c3ffa2018a5eca162a4a3db68809
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-26 18:30:36 +0900

    適当なボタンから接続してみるテスト

---
+++ Assets/Scripts/TestButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System;
+using UniRx.Operators;
+using Network;
+
+/// <summary>
+/// テスト用のボタン
+/// 用が済んだら消す
+/// </summary>
+public class TestButton : MonoBehaviour
+{
+    void Awake()
+    {
+        var Btn = GetComponent<Button>();
+        Btn.OnClickAsObservable()
+            .Subscribe((_) => ChatConnection.Intsance.Connect())
+            .AddTo(gameObject);
+    }
+}
