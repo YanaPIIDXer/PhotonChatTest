7782d536dcfce7bb960adfde421eab1f6e1becb6
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-26 17:57:31 +0900

    Githubで管理してはいけないデータを放り込むクラス partialクラスとして定義し、データの実態は.gitignoreに放り込んであるソースに定義したコンストラクタ内で放り込んでいる

---
+++ Assets/Scripts/Environments/EnvironmentVariables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environments
+{
+    /// <summary>
+    /// 環境変数定義
+    /// Githubで公開してはいけない値を扱うクラス
+    /// </summary>
+    public partial class EnvironmentVariables
+    {
+        /// <summary>
+        /// アプリケーションＩＤ
+        /// </summary>
+        public string ApplicationId { get; private set; }
+
+        #region Singleton
+        public static EnvironmentVariables Insatnce { get { return _Instance; } }
+        private static EnvironmentVariables _Instance = new EnvironmentVariables();
+        #endregion
+    }
+}
