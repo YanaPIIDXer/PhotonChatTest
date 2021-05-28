a668e2d2d7a6feabde8b66a069da878271a4babe
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-29 07:39:14 +0900

    チャンネルリストとボタン ※表示はまだ未実装

---
+++ Assets/Scripts/UI/Lobby/ChannelButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Lobby
+{
+    /// <summary>
+    /// チャンネルボタン
+    /// </summary>
+    public class ChannelButton : MonoBehaviour
+    {
+        /// <summary>
+        /// Prefabのパス
+        /// </summary>
+        private static readonly string PrefabPath = "Prefabs/UI/ChannelButton";
+
+        /// <summary>
+        /// Prefab
+        /// </summary>
+        private static GameObject Prefab = null;
+
+        /// <summary>
+        /// 生成
+        /// </summary>
+        /// <param name="ChannelName">チャンネル名</param>
+        /// <returns>ボタン</returns>
+        public static ChannelButton Create(string ChannelName)
+        {
+            if (Prefab == null)
+            {
+                Prefab = Resources.Load<GameObject>(PrefabPath);
+                Debug.Assert(Prefab != null, "Channel Button Prefab is NULL! Path:" + PrefabPath);
+            }
+
+            var Obj = Instantiate(Prefab);
+            return Obj.GetComponent<ChannelButton>();
+        }
+        
+        
+        /// <summary>
+        /// 表示テキスト
+        /// </summary>
+        [SerializeField]
+        private Text DisplayText = null;
+    }
+}
