1f629da0cbc7dc9f99504823f7f4499ac2c38723
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-29 10:18:41 +0900

    リストの中からチャンネルを1つ選択できるようにした

--- Assets/Scripts/UI/Lobby/ChannelList.cs
+++ Assets/Scripts/UI/Lobby/ChannelList.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using UniRx;
 
 namespace UI.Lobby
 {
@@ -14,6 +16,11 @@ namespace UI.Lobby
         /// リストビュー
         /// </summary>
         private ScrollRect ListView = null;
+
+        /// <summary>
+        /// 名前に対応するボタンのDictionary
+        /// </summary>
+        private Dictionary<string, ChannelButton> ButtonDic = new Dictionary<string, ChannelButton>();
 
         void Awake()
         {
@@ -30,6 +37,16 @@ namespace UI.Lobby
             ChBtn.transform.SetParent(ListView.content);
             ChBtn.transform.localPosition = Vector3.zero;
             ChBtn.Selected = false;
+            ButtonDic[ChannelName] = ChBtn;
+
+            ChBtn.OnPress
+                 .Subscribe((Name) =>
+                 {
+                     foreach (var Kv in ButtonDic)
+                     {
+                         Kv.Value.Selected = Kv.Key == Name;
+                     }
+                 });
         }
     }
 }
