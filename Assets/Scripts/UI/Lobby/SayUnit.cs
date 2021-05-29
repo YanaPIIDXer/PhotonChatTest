08c4a2d37e584e2fa029674cbe150a56b3a5ff18
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-29 11:32:26 +0900

    使用できないものは暗転させるようにした

--- Assets/Scripts/UI/Lobby/SayUnit.cs
+++ Assets/Scripts/UI/Lobby/SayUnit.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+using System;
 
 namespace UI.Lobby
 {
@@ -21,5 +24,28 @@ namespace UI.Lobby
         /// </summary>
         [SerializeField]
         private Button SayButton = null;
+
+        /// <summary>
+        /// 現在のチャンネル名
+        /// </summary>
+        public string CurrentChannel
+        {
+            get { return _CurrentChannel; }
+            set
+            {
+                _CurrentChannel = value;
+                MessageInput.interactable = !string.IsNullOrEmpty(_CurrentChannel);
+            }
+        }
+        private string _CurrentChannel = "";
+
+        void Awake()
+        {
+            CurrentChannel = "";
+            MessageInput.OnValueChangedAsObservable()
+                        .Select((Value) => string.IsNullOrEmpty(Value))
+                        .Subscribe((IsEmpty) => SayButton.interactable = !IsEmpty)
+                        .AddTo(gameObject);
+        }
     }
 }
