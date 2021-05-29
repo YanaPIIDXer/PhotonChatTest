00fc4d629d165507ab04d4bbdcc43ad899bb1916
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-29 11:24:25 +0900

    発言ユニットのUI配置とComponentをとりあえず定義

---
+++ Assets/Scripts/UI/Lobby/SayUnit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Lobby
+{
+    /// <summary>
+    /// 発言ユニット
+    /// </summary>
+    public class SayUnit : MonoBehaviour
+    {
+        /// <summary>
+        /// メッセージ入力欄
+        /// </summary>
+        [SerializeField]
+        private InputField MessageInput = null;
+
+        /// <summary>
+        /// 発言ボタン
+        /// </summary>
+        [SerializeField]
+        private Button SayButton = null;
+    }
+}
