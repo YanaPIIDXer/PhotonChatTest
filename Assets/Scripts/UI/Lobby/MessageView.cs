6d58b72118d1321bf1afd30ed36fd96d82f6545d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-29 10:44:04 +0900

    メッセージビューのUI配置とComponentをとりあえず定義

---
+++ Assets/Scripts/UI/Lobby/MessageView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI.Lobby
+{
+    /// <summary>
+    /// メッセージビュー
+    /// </summary>
+    public class MessageView : MonoBehaviour
+    {
+    }
+}
